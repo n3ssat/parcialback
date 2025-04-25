2776a510037e42e4452f117a6843047240043f68
Jenny <vannej321@gmail.com>
2025-04-24 21:05:18 -0500

    Se arreglaron los link

--- server.js
+++ server.js
@@ -22,5 +22,6 @@ const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log('🚀 Conectado a MongoDB');
+        app.listen(PORT, () => console.log(`🔥 Servidor corriendo en http://localhost:${PORT}`));
     })
     .catch(err => console.error('Error al conectar a MongoDB:', err));
